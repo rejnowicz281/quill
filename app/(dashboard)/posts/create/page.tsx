c467889edfc7cfecf248cad01bf70c9d17e856ad
Alan Rejnowicz <alanrejnowicz281@gmail.com>
2024-09-14 13:23:15 +0200

    Padding change

--- app/(dashboard)/posts/create/page.tsx
+++ app/(dashboard)/posts/create/page.tsx
@@ -5,7 +5,7 @@ import PostForm from "@/components/posts/form";
 
 export default function CreatePostPage() {
     return (
-        <div className="flex flex-col flex-1">
+        <div className="flex flex-col flex-1 py-8">
             <PageTitle>Create Post</PageTitle>
             <PostForm />
         </div>
